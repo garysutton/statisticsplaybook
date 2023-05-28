8421eb92110bd7b8592565f0da7ba3a9c9ef8002
garysutton <garysutton@users.noreply.github.com>
2023-05-28 18:01:20 -0400

    Add files via upload

--- CH20.R
+++ CH20.R
@@ -1,10 +1,10 @@
 
 library(networkD3)
 
-nodes <- data.frame("name" = c("cluster analysis",
-                               "base", "factoextra",
-                               "hierarchical clustering", "k-means clustering",
-                               "3", "11"))
+nodes <- data.frame('name' = c('cluster analysis',
+                               'base', 'factoextra',
+                               'hierarchical clustering', 'k-means clustering',
+                               '3', '11'))
 links <- as.data.frame(matrix(c(
   0,1,50, 0,2,50,
   1,3,50, 1,4,50,
@@ -12,18 +12,18 @@ links <- as.data.frame(matrix(c(
   3,5,50,
   4,6,50),
   byrow = TRUE, ncol = 3))
-names(links) = c("source", "target", "value")
+names(links) = c('source', 'target', 'value')
 
 sankeyNetwork(Links = links, Nodes = nodes,
-              Source = "source", Target = "target",
-              Value = "value", NodeID = "name",
+              Source = 'source', Target = 'target',
+              Value = 'value', NodeID = 'name',
               fontSize = 12, nodeWidth = 30)
 
-nodes <- data.frame("name" = c("significance testing",
-                               "base", "ggpubr", "gtools", "vcd",
-                               "chi-square test", "correlation test", 
-                               "f-test", "t-test",
-                               "7", "9", "10", "12", "13", "14"))
+nodes <- data.frame('name' = c('significance testing',
+                               'base', 'ggpubr', 'gtools', 'vcd',
+                               'chi-square test', 'correlation test', 
+                               'f-test', 't-test',
+                               '7', '9', '10', '12', '13', '14'))
 links <- as.data.frame(matrix(c(
   0,1,50, 0,2,50, 0,3,50, 0,4,50,
   1,5,50, 1,6,50, 1,7,50, 1,8,50, 
@@ -35,14 +35,14 @@ links <- as.data.frame(matrix(c(
   7,13,50,
   8,9,50, 8,12,50, 8,13,50),
   byrow = TRUE, ncol = 3))
-names(links) = c("source", "target", "value")
+names(links) = c('source', 'target', 'value')
 
 sankeyNetwork(Links = links, Nodes = nodes,
-              Source = "source", Target = "target",
-              Value = "value", NodeID = "name",
+              Source = 'source', Target = 'target',
+              Value = 'value', NodeID = 'name',
               fontSize = 12, nodeWidth = 30)
 
-nodes <- data.frame("name" = c("effect size testing",
+nodes <- data.frame('name' = c("effect size testing",
                                "effectsize", "effsize", "questionr", "rcompanion",
                                "cohen's d", "cramer's v", "glass's delta", "hedges' g",
                                "7", "9", "12", "13"))
@@ -57,19 +57,19 @@ links <- as.data.frame(matrix(c(
   7,12,50,
   8,12,50),
   byrow = TRUE, ncol = 3))
-names(links) = c("source", "target", "value")
+names(links) = c('source', 'target', 'value')
 
 sankeyNetwork(Links = links, Nodes = nodes,
-              Source = "source", Target = "target",
-              Value = "value", NodeID = "name",
+              Source = 'source', Target = 'target',
+              Value = 'value', NodeID = 'name',
               fontSize = 12, nodeWidth = 30)
 
-nodes <- data.frame("name" = c("modeling",
-                               "base", "broom", "car", "caret", "InformationValue", "pscl", "questionr",
-                               "SciViews", "tree", 
-                               "analysis of variance", "linear regression", 
-                               "logistic regression", "regression tree",
-                               "5", "14"))
+nodes <- data.frame('name' = c('modeling',
+                               'base', 'broom', 'car', 'caret', 'InformationValue', 'pscl', 'questionr',
+                               'SciViews', 'tree', 
+                               'analysis of variance', 'linear regression', 
+                               'logistic regression', 'regression tree',
+                               '5', '14'))
 links <- as.data.frame(matrix(c(
   0,1,50, 0,2,50, 0,3,50, 0,4,50, 0,5,50, 0,6,60, 0,7,50, 0,8,50, 0,9,50, 
   1,10,50, 1,11,50, 1,12,50, 
